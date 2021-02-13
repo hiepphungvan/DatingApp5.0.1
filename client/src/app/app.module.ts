c8dd63c07466fac136e4c8f7832382caf2d589ce
Phung Van Hiep <hiepphungvan@gmail.com>
2021-02-14 01:11:24 +0900

    End of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,11 +21,25 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
-    ,NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent, TestErrorsComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent
+    ,NavComponent
+    ,HomeComponent
+    , RegisterComponent
+    , MemberListComponent
+    , MemberDetailComponent
+    , ListsComponent
+    , MessagesComponent
+    , TestErrorsComponent
+    , NotFoundComponent
+    , ServerErrorComponent
+    , MemberCardComponent
+    , MemberEditComponent
     
   ],
   imports: [
@@ -34,11 +48,13 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
 ],
   bootstrap: [AppComponent]
 })
